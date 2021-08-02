b4baa6e4cd598553696235a65947f91aae91734e
Ali Bulut <ali@alibulut.net>
2021-08-02 21:43:29 +0300

    initial commit

---
+++ RabbitMQWatermarkApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RabbitMQWatermarkApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
